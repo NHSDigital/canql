19ef8efc13944639fa8af27214570290d282ff3e
Maximilian Wasylow <maxw@maxpc.magog.ecric.org.uk>
2017-02-15 15:02:23 +0000

    Rubocop compliance

--- test/nodes/patient_test.rb
+++ test/nodes/patient_test.rb
@@ -109,4 +109,60 @@ class PatientTest < Minitest::Test
     assert_array_includes parser_v2.meta_data['mother.fields_populated'][Canql::EQUALS],
                           %w(postcode birthdate)
   end
+
+  def test_dob_alias_valid
+    query1 = 'all babies with missing date of birth'
+    parser = Canql::Parser.new(query1)
+    message = "That's a reference test for 'dob' alias, if this fails,\
+               something is wrong!!!"
+    assert(parser.valid?, message)
+
+    query1 = 'all babies with missing dob'
+    parser = Canql::Parser.new(query1)
+    message = '\'dob\' should be a valid alias for \'date of birth\'!'
+    assert(parser.valid?, message)
+  end
+
+  def test_dob_alias_simple_query
+    query1 = 'all babies with missing postcode, date of birth'
+    query2 = 'all babies with missing postcode, dob'
+    parser1 = Canql::Parser.new(query1)
+    parser2 = Canql::Parser.new(query2)
+    message = '\'dob\' doesn\'t work as an alias of \'date of birth\'!'
+    assert_equal(parser1.meta_data, parser2.meta_data, message)
+
+    query1 = 'all babies with missing postcode, date of birth'
+    query2 = 'all babies with missing postcode, pac'
+    parser1 = Canql::Parser.new(query1)
+    parser2 = Canql::Parser.new(query2)
+    message = "'pac' appears to be working in the same way as 'dob' does,\
+               and 'pac' wasn't included in the grammar config!"
+    assert(parser1.meta_data != parser2.meta_data, message)
+  end
+
+  def test_dob_alias_complicated_query
+    query1 = 'first 27 male liveborn thames cases \
+      expected between 20/06/2015 and 25/06/2015 \
+      and born on 22/06/2015 and that died on 01/12/2015 \
+      with prenatal anomalies \
+      and postnatal tests and missing postcode and date of birth \
+      and wait action and unprocessed paediatric records \
+      and mother born between 01/10/1990 and 10/01/1999 \
+      and who died on 01/01/2016 \
+      with fields postcode and nhs number'
+    query2 = 'first 27 male liveborn thames cases \
+      expected between 20/06/2015 and 25/06/2015 \
+      and born on 22/06/2015 and that died on 01/12/2015 \
+      with prenatal anomalies \
+      and postnatal tests and missing postcode and dob \
+      and wait action and unprocessed paediatric records \
+      and mother born between 01/10/1990 and 10/01/1999 \
+      and who died on 01/01/2016 \
+      with fields postcode and nhs number'
+    parser1 = Canql::Parser.new(query1)
+    parser2 = Canql::Parser.new(query2)
+    message = "'dob' doesn't work as an alias of 'date of birth'\
+      in a complicated query!"
+    asser_equal(parser1.meta_data, parser2.meta_data, message)
+  end
 end
